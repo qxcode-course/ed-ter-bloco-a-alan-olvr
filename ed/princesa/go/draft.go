c484277acb6f78224d494059ba7988522c4aa672
Alan Oliveira <alanoliveira.0199@gmail.com>
2026-03-11 14:19:04 +0000

    em andamento

---
+++ ed/princesa/go/draft.go
@@ -0,0 +1,17 @@
+package main
+import "fmt"
+func main() {
+
+    var n int
+    fmt.Scanln(&n)
+    var e int
+    fmt.Scanln(&e)
+
+    var elementos [n]int
+
+    i := 1
+    for i <= n {
+        fmt.Scanln(elementos)
+    }
+
+}
