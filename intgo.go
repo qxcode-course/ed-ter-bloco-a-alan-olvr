7174b92a6fef3547668db16300df48ebf5f06174
Alan Oliveira <alanoliveira.0199@gmail.com>
2026-03-11 14:28:46 +0000

    feito o "hello world" e trabalho com valores

---
+++ intgo.go
@@ -0,0 +1,18 @@
+package main
+
+import "fmt"
+
+/* hello world
+func main(){
+	fmt.Println("Hello World")
+}
+*/
+
+func main(){
+	
+	fmt.Println("go" + "lang")
+	fmt.Println("1+1 = ", 1+1)
+	fmt.Println("8/4 = ", 8.0/4.0)
+	
+}
+
