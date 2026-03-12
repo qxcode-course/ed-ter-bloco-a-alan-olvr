0bc90ee95ecffd62685a42dc3b7ec900df6b6a8a
Alan Oliveira <alanoliveira.0199@gmail.com>
2026-03-12 20:52:36 +0000

    atividade "pintando" feita

---
+++ ed/pintando/go/draft.go
@@ -0,0 +1,16 @@
+package main
+import "fmt"
+import "math"
+func main() {
+
+    var n1, n2, n3 float64
+    fmt.Scanln(&n1)
+    fmt.Scanln(&n2)
+    fmt.Scanln(&n3)
+
+    p := (n1 + n2 + n3)/2.0
+    area := math.Sqrt(p*(p - n1)*(p - n2)*(p - n3))
+
+    fmt.Printf("%.2f", area)
+
+}
