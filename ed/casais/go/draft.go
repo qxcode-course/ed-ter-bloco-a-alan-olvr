549b931ec4a72a11b680c7c369155050db34a258
Alan Oliveira <alanoliveira.0199@gmail.com>
2026-03-21 02:03:45 +0000

    atividade "casais" pendente

--- ed/casais/go/draft.go
+++ ed/casais/go/draft.go
@@ -6,12 +6,32 @@ func main() {
 
 	var n int
 	fmt.Scanln(&n)
-	var vet [n]int
-
+	var vet []int
+	var usado []bool
 	var qtdPares int
 
-	for i := 1; i < n; i++ {
-		var animal int
+	for i := 0; i < n; i++ {
 
+		var animal int
+		fmt.Scanln(&animal)
+
+		pos := -1
+
+		for j := 0; j < len(vet); j++ {
+			if !usado[j] && vet[j] == -animal {
+				pos = j
+				break
+			}
+		}
+
+		if pos != -1 {
+			usado[pos] = true
+			qtdPares++
+		} else {
+			vet = append(vet, animal)
+			usado = append(usado, false)
+		}
 	}
+
+	fmt.Println(qtdPares)
 }
