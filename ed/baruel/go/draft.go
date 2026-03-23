e16452cfc2cc20c6e8df1f3e60f49a529094929a
Alan Oliveira <alanoliveira.0199@gmail.com>
2026-03-23 02:28:03 +0000

    atividade "baruel" concluída

--- ed/baruel/go/draft.go
+++ ed/baruel/go/draft.go
@@ -1,5 +1,68 @@
 package main
 import "fmt"
-func main() {
-    fmt.Println("qxcode")
+
+type Colecao struct {
+	TotalAlbum int
+	Possuidas  []int
 }
+
+func main() {
+	var c Colecao
+	var qtdPossui int
+
+	fmt.Scan(&c.TotalAlbum)
+	fmt.Scan(&qtdPossui)
+
+	for i := 0; i < qtdPossui; i++ {
+		var fig int
+		fmt.Scan(&fig)
+		c.Possuidas = append(c.Possuidas, fig)
+	}
+
+	teveRepetida := false
+	primeiraRepetida := true
+
+	for i := 1; i < len(c.Possuidas); i++ {
+		if c.Possuidas[i] == c.Possuidas[i-1] {
+			if !primeiraRepetida {
+				fmt.Print(" ")
+			}
+			fmt.Print(c.Possuidas[i])
+			teveRepetida = true
+			primeiraRepetida = false
+		}
+	}
+
+	if !teveRepetida {
+		fmt.Print("N")
+	}
+	fmt.Println() 
+
+	teveFaltante := false
+	primeiraFaltante := true
+
+	for i := 1; i <= c.TotalAlbum; i++ {
+		encontrou := false
+		for _, fig := range c.Possuidas {
+			if fig == i {
+				encontrou = true
+				break
+			}
+		}
+
+		if !encontrou {
+			if !primeiraFaltante {
+				fmt.Print(" ")
+			}
+			fmt.Print(i)
+			teveFaltante = true
+			primeiraFaltante = false
+		}
+	}
+
+	if !teveFaltante {
+		fmt.Print("N")
+	}
+	fmt.Println()
+}
+
