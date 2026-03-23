7982fa8440087a2fa9e2881e8378bc770b9d68d4
Alan Oliveira <alanoliveira.0199@gmail.com>
2026-03-23 02:57:35 +0000

    atividade "gomos" concluida

--- ed/gomos/go/draft.go
+++ ed/gomos/go/draft.go
@@ -1,5 +1,39 @@
 package main
+
 import "fmt"
+
+type Ponto struct {
+	X int
+	Y int
+}
+
 func main() {
-    fmt.Println("qxcode")
+	var q int
+	var direcao string
+
+	fmt.Scan(&q, &direcao)
+	cobra := make([]Ponto, q)
+
+	for i := 0; i < q; i++ {
+		fmt.Scan(&cobra[i].X, &cobra[i].Y)
+	}
+
+	for i := q - 1; i > 0; i-- {
+		cobra[i] = cobra[i-1]
+	}
+
+	switch direcao {
+	case "L":
+		cobra[0].X--
+	case "R":
+		cobra[0].X++
+	case "U":
+		cobra[0].Y--
+	case "D":
+		cobra[0].Y++
+	}
+
+	for i := 0; i < q; i++ {
+		fmt.Printf("%d %d\n", cobra[i].X, cobra[i].Y)
+	}
 }
