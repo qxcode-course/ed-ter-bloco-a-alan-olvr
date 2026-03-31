409a667be641aae5ca3c499943a81bfbffe71403
Alan Oliveira <alanoliveira.0199@gmail.com>
2026-03-31 01:00:13 +0000

    atividade "princesa" concluída

--- ed/princesa/go/draft.go
+++ ed/princesa/go/draft.go
@@ -1,17 +1,59 @@
 package main
+
 import "fmt"
+
+func josephus(n int, e int) {
+
+	elementos := make([]int, n)
+	vivos := make([]bool, n)
+	for i := 0; i < n; i++ {
+		elementos[i] = i + 1
+		vivos[i] = true
+	}
+
+	espada := e - 1
+	count := n
+
+	for count > 1 {
+		fmt.Print("[ ")
+		for i := 0; i < n; i++ {
+			if vivos[i] {
+				fmt.Printf("%d", elementos[i])
+				if i == espada {
+					fmt.Print(">")
+				}
+				fmt.Print(" ")
+			}
+		}
+		fmt.Println("]")
+
+		alvo := (espada + 1) % n
+		for !vivos[alvo] {
+			alvo = (alvo + 1) % n
+		}
+
+		vivos[alvo] = false
+		count--
+
+		espada = (alvo + 1) % n
+		for !vivos[espada] {
+			espada = (espada + 1) % n
+		}
+	}
+
+	for i := 0; i < n; i++ {
+		if vivos[i] {
+			fmt.Printf("[ %d> ]\n", elementos[i])
+			break
+		}
+	}
+}
+
 func main() {
 
-    var n int
-    fmt.Scanln(&n)
-    var e int
-    fmt.Scanln(&e)
+	var n, e int
+	fmt.Scan(&n, &e)
 
-    var elementos [n]int
-
-    i := 1
-    for i <= n {
-        fmt.Scanln(elementos)
-    }
+	josephus(n, e)
 
 }
