8e2f5a0e5dc3b3d14970e5dd68a59fbf37168dab
Alan Oliveira <alanoliveira.0199@gmail.com>
2026-04-07 00:29:23 +0000

    atividades encaminhadas

--- ed/resto/src/go/draft.go
+++ ed/resto/src/go/draft.go
@@ -1,5 +1,17 @@
 package main
 import "fmt"
+
+func divisao(n int) {
+    if n == 0 {
+        return
+    } 
+
+    divisao(n / 2) 
+    fmt.Println(n/2, n%2)
+}
+
 func main() {
-    fmt.Println("Hello, World!")
+    var n int
+    fmt.Scan(&n)
+    divisao(n)
 }
