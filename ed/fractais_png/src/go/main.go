34c7e9f9fd1c3d3a7338dfbc9709fce2b2e39907
Alan Oliveira <alanoliveira.0199@gmail.com>
2026-04-08 14:20:48 +0000

    fractais em andamento

--- ed/fractais_png/src/go/main.go
+++ ed/fractais_png/src/go/main.go
@@ -9,52 +9,37 @@ func randInt(min, max int) int {
 	return min + rand.Intn(max-min+1)
 }
 
-func arvore(pen *Pen, dist float64) {
-	if dist < 10 {
+func circulos(pen *Pen, raio float64, nivel int) {
+	//condição de parada
+	if nivel == 0 {
 		return
 	}
-	ang := 30.0
-	fator := 0.9
-	pen.SetLineWidth(dist / 5)
-	pen.Walk(dist)
-	pen.Right(ang)
-	arvore(pen, dist*fator)
-	pen.Right(-2 * ang)
-	arvore(pen, dist*fator)
-	pen.Right(ang)
-	pen.Walk(-dist)
+
+	//desenhando o circulo central
+	pen.DrawCircle(raio)
+
+	for i := 0; i < 6; i++ {
+		pen.Up()
+		pen.Walk(raio)
+		pen.Down()
+		circulos(pen, raio/2.5, nivel-1)
+		pen.Up()
+		pen.Walk(-raio)
+		pen.Down()
+		pen.Left(60)
+	}
+
 }
 
+
 func main() {
-	pen := NewPen(600, 500)   // cria um canvas de 500 de largura por 500 de altura
-	pen.SetRGB(255, 0, 0)     // muda a cor do pincel para vermelho
-	pen.SetPosition(250, 500) // move o pincel para x 250, y 500
-	pen.SetHeading(90)
-	arvore(pen, 150)
+	pen := NewPen(800, 800)   // cria um canvas de 500 de largura por 500 de altura
+	pen.SetRGB(255, 255, 255)     // muda a cor do pincel para vermelho
+	pen.SetPosition(400, 400) // move o pincel para x 250, y 500
+	circulos(pen, 200, 4)
+	pen.SetHeading(90) // coloca o pincel apontando para cima
+	
 
-	// coloca o pincel apontando para cima
-	/*pen.Walk(100)             // anda 100 pixels
-	pen.Left(30)              // dobra 30 graus para esquerda
-	pen.Walk(100)             // anda 100 pixels
-	pen.DrawCircle(50)        // desenha um círculo de raio 50
-	pen.Right(60)             // gira para direita 60 graus
-	pen.Walk(150)
-	*/
-
-	/*for range 10 {
-		pen.Up()
-		pen.Walk(30) // anda sem riscar
-		pen.Down()
-
-		pen.DrawCircle(10) //desenha um circulo pequeno
-
-		pen.Up()
-		pen.Walk(-30) // volta sem riscar
-		pen.Down()
-
-		pen.Left(36) // gira
-	}*/
-
-	pen.SavePNG("tree.png")
+	pen.SavePNG("circulos.png")
 	fmt.Println("PNG file created successfully.")
 }
