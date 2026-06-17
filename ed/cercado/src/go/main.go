0a8fd9fc074445a182387ff59672fa52d411ffb0
Alan Oliveira <alanoliveira.0199@gmail.com>
2026-06-17 19:37:10 +0000

    atividades atualizadas

--- ed/cercado/src/go/main.go
+++ ed/cercado/src/go/main.go
@@ -8,7 +8,42 @@ import (
 
 // NÃO ALTERE A ASSINATURA DA FUNÇÃO solve
 func solve(board [][]byte) {
-	_ = board
+	if len(board) == 0 {
+		return
+	}
+	nrows := len(board)
+	ncols := len(board[0])
+
+	var dfs func(r, c int)
+	dfs = func(r, c int) {
+		if r < 0 || r >= nrows || c < 0 || c >= ncols || board[r][c] != 'O' {
+			return
+		}
+		board[r][c] = 'S'
+		dfs(r+1, c)
+		dfs(r-1, c)
+		dfs(r, c+1)
+		dfs(r, c-1)
+	}
+
+	for r := 0; r < nrows; r++ {
+		dfs(r, 0)
+		dfs(r, ncols-1)
+	}
+	for c := 0; c < ncols; c++ {
+		dfs(0, c)
+		dfs(nrows-1, c)
+	}
+
+	for r := 0; r < nrows; r++ {
+		for c := 0; c < ncols; c++ {
+			if board[r][c] == 'O' {
+				board[r][c] = 'X'
+			} else if board[r][c] == 'S' {
+				board[r][c] = 'O'
+			}
+		}
+	}
 }
 
 // NÃO ALTERE A MAIN
---
+++ ed/pintura/src/go/main.go
@@ -0,0 +1,76 @@
+package main
+
+import (
+	"bufio"
+	"fmt"
+	"os"
+	"strconv"
+	"strings"
+)
+
+// Não modifique a assinatura da função floodFill
+func floodFill(image [][]int, sr int, sc int, color int) [][]int {
+	original := image[sr][sc]
+	if original == color {
+		return image
+	}
+
+	var dfs func(r, c int)
+	dfs = func(r, c int) {
+		if r < 0 || r >= len(image) || c < 0 || c >= len(image[0]) {
+			return
+		}
+		if image[r][c] != original {
+			return
+		}
+		image[r][c] = color
+		dfs(r+1, c)
+		dfs(r-1, c)
+		dfs(r, c+1)
+		dfs(r, c-1)
+	}
+
+	dfs(sr, sc)
+	return image
+}
+
+// Não modifique a função main
+func main() {
+	scanner := bufio.NewScanner(os.Stdin)
+	scanner.Scan()
+	line := scanner.Text()
+	parts := strings.Fields(line)
+	nl, _ := strconv.Atoi(parts[0])
+	nc, _ := strconv.Atoi(parts[1])
+
+	image := make([][]int, nl)
+	for i := 0; i < nl; i++ {
+		scanner.Scan()
+		rowStr := strings.Fields(scanner.Text())
+		row := make([]int, nc)
+		for j := 0; j < nc; j++ {
+			row[j], _ = strconv.Atoi(rowStr[j])
+		}
+		image[i] = row
+	}
+
+	// Lê sr, sc e color
+	scanner.Scan()
+	params := strings.Fields(scanner.Text())
+	sr, _ := strconv.Atoi(params[0])
+	sc, _ := strconv.Atoi(params[1])
+	color, _ := strconv.Atoi(params[2])
+
+	result := floodFill(image, sr, sc, color)
+
+	// Imprime a matriz resultante
+	for _, row := range result {
+		for j, val := range row {
+			if j > 0 {
+				fmt.Print(" ")
+			}
+			fmt.Print(val)
+		}
+		fmt.Println()
+	}
+}
