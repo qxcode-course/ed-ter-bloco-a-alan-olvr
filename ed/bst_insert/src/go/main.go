7bca9c0d898cd4c606ba8882abc51830c334d31d
Alan Oliveira <alanoliveira.0199@gmail.com>
2026-07-08 02:56:49 +0000

    Atualizações feitas

---
+++ ed/bst_insert/src/go/main.go
@@ -0,0 +1,80 @@
+package main
+
+import (
+	"bufio"
+	"fmt"
+	"os"
+	"strconv"
+	"strings"
+)
+
+type Node struct {
+	Value int
+	Left  *Node
+	Right *Node
+}
+
+func BstInsert(values []int) *Node {
+	var root *Node
+	for _, value := range values {
+		root = insertOne(root, value)
+	}
+	return root
+}
+
+func insertOne(node *Node, value int) *Node {
+	if node == nil {
+		return &Node{Value: value}
+	}
+	if value < node.Value {
+		node.Left = insertOne(node.Left, value)
+	} else if value > node.Value{
+		node.Right = insertOne(node.Right, value)
+	}
+
+	return node
+}
+
+// -----------------------------------------------------------------------------------
+func BShow(node *Node, history string) {
+	if node != nil && (node.Left != nil || node.Right != nil) {
+		BShow(node.Left, history+"l")
+	}
+	for i := 0; i < len(history)-1; i++ {
+		if history[i] != history[i+1] {
+			fmt.Print("│   ")
+		} else {
+			fmt.Print("    ")
+		}
+	}
+	if history != "" {
+		if history[len(history)-1] == 'l' {
+			fmt.Print("╭───")
+		} else {
+			fmt.Print("╰───")
+		}
+	}
+	if node == nil {
+		fmt.Println("#")
+		return
+	}
+	fmt.Println(node.Value)
+	if node.Left != nil || node.Right != nil {
+		BShow(node.Right, history+"r")
+	}
+}
+
+func main() {
+	scanner := bufio.NewScanner(os.Stdin)
+	scanner.Scan()
+	parts := strings.Split(scanner.Text(), " ")
+	values := make([]int, 0, len(parts))
+	for _, elem := range parts {
+		v, err := strconv.Atoi(elem)
+		if err == nil {
+			values = append(values, v)
+		}
+	}
+	root := BstInsert(values)
+	BShow(root, "") // Chama a função de impressão formatada
+}
